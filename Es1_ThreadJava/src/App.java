64cdda7b13e82590d5d07e3c2b44d8a7d72f8463
lai2-01 <lai2-01@itismeucci.com>
2022-03-28 12:54:13 +0200

    Secondo caricamento

--- Es1_ThreadJava/src/App.java
+++ Es1_ThreadJava/src/App.java
@@ -1,8 +1,11 @@
 public class App {
     public static void main(String[] args) throws Exception {
         ContaINani contanani = new ContaINani();
+        ContaINani.sottoNani1 sottoNano1 = contanani.new sottoNani1();
+        ContaINani.sottoNani2 sottoNano2 = contanani.new sottoNani2();
         System.out.println("Sono nel MAIN - - >");
-        contanani.run();
+        sottoNano1.run();
+        sottoNano2.run2();
         System.out.println("Fine!");
     }
 }
--- Es1_ThreadJava/src/ContaINani.java
+++ Es1_ThreadJava/src/ContaINani.java
@@ -1,9 +1,24 @@
-public class ContaINani extends Thread{
+public class ContaINani{
     
-    public void run(){
-        System.out.println("Sono dentro il ciclo dei nani!");
-        for(int i = 0; i < 7; i++){
-            System.out.println("Nano: " + (i+1) + ";" ) ;
+    class sottoNani1 extends Thread{
+        public void run(){
+            System.out.println("Sono dentro il ciclo dei nani 1!" + getName());
+            for(int i = 0; i < 7; i++){
+                System.out.println("Nano: " + (i+1) + ";" ) ;
+            }
         }
     }
-}
+
+    public class sottoNani2 extends Thread{
+        public void run2(){
+            System.out.println("Sono dentro il ciclo dei nani numero 2!" + getName());
+            for(int i = 0; i < 7; i++){
+                System.out.println("Nani: " + (i+1) + ";" ) ;
+            }
+        }
+    }
+
+    
+    }
+    
+
